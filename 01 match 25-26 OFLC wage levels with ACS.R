b8e081d7cdf4ec367f791eb2c948ff8cbd041329
co237 <55267461+co237@users.noreply.github.com>
2026-02-12 14:34:24 -0500

    change date

--- 01 match 25-26 OFLC wage levels with ACS.R
+++ 01 match 25-26 OFLC wage levels with ACS.R
@@ -1,4 +1,5 @@
 # Tag OFLC occupations with the best ACS OCCSOC code to match with microdata.
+setwd("~/Documents/Github/acs_wage_level_alternative")
 
 # Load ACS data
 ddi <- read_ipums_ddi("usa_00069.xml")
@@ -51,7 +52,7 @@ occupation_totals <- occupation_totals %>%
 oflc_wage_levels_25_26<- read.csv("OFLC_Wages_2025-26_Updated/ALC_Export.csv")
 
 # Create a list of all the occupations for which OFLC produces wage levels
-oflc_occupations_list <- oflc_wage_levels_24_25 %>%
+oflc_occupations_list <- oflc_wage_levels_25_26 %>%
   group_by(SocCode) %>%
   summarise(n = n())
 
@@ -72,23 +73,43 @@ direct_matches <- oflc_occupations_list %>%
   select(-SOC2, -SOC3, -SOC4, -total)
 
 secondary_matches <- oflc_occupations_list %>%
+  filter(!(SocCode %in% direct_matches$SocCode)) %>%
   left_join(occupation_totals, by = c("SOC Broad Occupation" = "SOC2")) %>%
   filter(!is.na(OCCSOC)) %>%
   mutate(ACS_occupation = OCCSOC) %>%
   select(-Full_SOC, -SOC3, -SOC4, -total)
 
 tertiary_matches <- oflc_occupations_list %>%
+  filter(!(SocCode %in% direct_matches$SocCode),
+         !(SocCode %in% secondary_matches$SocCode)) %>%
   left_join(occupation_totals, by = c("SOC Minor Occupation" = "SOC3")) %>%
   filter(!is.na(OCCSOC)) %>%
   mutate(ACS_occupation = OCCSOC) %>%
   select(-Full_SOC, -SOC2, -SOC4, -total)
 
+direct_matches %>% select(SocCode) %>% distinct() %>% summarise(n = n())
+secondary_matches %>% select(SocCode) %>% distinct() %>% summarise(n = n())
+tertiary_matches %>% select(SocCode) %>% distinct() %>% summarise(n = n())
+quaternary_matches %>% select(SocCode) %>% distinct() %>% summarise(n = n())
+377+52+173+24
 
 quaternary_matches <- oflc_occupations_list %>%
+  filter(!(SocCode %in% direct_matches$SocCode),
+         !(SocCode %in% secondary_matches$SocCode),
+         !(SocCode %in% tertiary_matches$SocCode)) %>%
   left_join(occupation_totals, by = c("SOC Minor Occupation" = "SOC4")) %>%
   filter(!is.na(OCCSOC)) %>%
   mutate(ACS_occupation = OCCSOC) %>%
   select(-Full_SOC, -SOC2, -SOC3, -total)
+
+no_matches <- oflc_occupations_list %>%
+  filter(!(SocCode %in% direct_matches$SocCode),
+         !(SocCode %in% secondary_matches$SocCode),
+         !(SocCode %in% tertiary_matches$SocCode),
+         !(SocCode %in% quaternary_matches$SocCode)) %>%
+  mutate(alt_SocCode = str_replace(SocCode, ".$", "0"))
+
+no_matches2 <- no_matches %>% filter(!(alt_SocCode %in% occupation_totals$OCCSOC))
 
 manual_matches <- oflc_occupations_list %>%
   mutate(OCCSOC = case_when(SocCode == "11-9131" ~ "11-91XX",
@@ -111,12 +132,133 @@ manual_matches <- oflc_occupations_list %>%
   filter(!is.na(ACS_occupation))
 
 
-
 combined_occupations <- rbind(direct_matches, secondary_matches,
                               tertiary_matches, quaternary_matches,
                               manual_matches)
 
+oflc_occupations_list %>% filter(!(SocCode %in% combined_occupations$SocCode))
+
 # Check that there are no OFLC codes that aren't assigned an ACS code
 
 combined_occupations %>% filter(is.na(ACS_occupation))
+
+write.csv(combined_occupations, "OFLC_to_ACS_crosswalk.csv")
+
+
+
+
+oflc_alt <- oflc_occupations_list %>%
+  select(-n, -`SOC Broad Occupation`, -`SOC Minor Occupation`) %>%
+  mutate(Soc_last1 = str_replace(SocCode, ".$", "0"),
+         Soc_last2 = str_replace(SocCode, "..$", "00"),
+         Soc_last3 = str_replace(SocCode, "...$", "000"))
+
+occupation_totals_alt <- occupation_totals %>%
+  mutate(Soc_last1 = str_replace(OCCSOC, ".$", "0"),
+         Soc_last2 = str_replace(OCCSOC, "..$", "00"),
+         Soc_last3 = str_replace(OCCSOC, "...$", "000"))
+
+write.csv(oflc_alt, "oflc_occupations.csv")
+write.csv(acs_occupations_alt, "acs_occupations.csv")
+
+acs_occupations_alt <- occupation_totals_alt %>%
+  select(-total, -SOC2, -SOC3, -SOC4, -Full_SOC)
+
+# Full code by Full code
+join_1 <- left_join(oflc_alt, occupation_totals_alt, by = c("SocCode" = "Full_SOC")) %>%
+  filter(!is.na(OCCSOC))
+
+join_2 <- oflc_alt %>%
+  filter(!(SocCode %in% join_1$SocCode)) %>%
+  left_join(occupation_totals_alt, by = c("Soc_last1" = "Soc_last1")) %>%
+  filter(!is.na(OCCSOC))
+
+join_3 <- oflc_alt %>%
+  filter(!(SocCode %in% join_1$SocCode),
+         !(SocCode %in% join_2$SocCode)) %>%
+  left_join(occupation_totals_alt, by = c("Soc_last2" = "Soc_last2")) %>%
+  filter(!is.na(OCCSOC))
+
+join_4 <- oflc_alt %>%
+  filter(!(SocCode %in% join_1$SocCode),
+         !(SocCode %in% join_2$SocCode),
+         !(SocCode %in% join_3$SocCode)) %>%
+  left_join(occupation_totals_alt, by = c("Soc_last3" = "Soc_last3")) %>%
+  filter(!is.na(OCCSOC))
+
+
+
+
+
+join_1 <- left_join(oflc_alt, occupation_totals, by = c("SocCode" = "Full_SOC")) %>%
+  filter(!is.na(OCCSOC))
+
+join_2 <- oflc_alt %>%
+  filter(!(SocCode %in% join_1$SocCode)) %>%
+  left_join(occupation_totals, by = c("Soc_last1" = "SOC2")) %>%
+  filter(!is.na(OCCSOC))
+join_3 <- oflc_alt %>%
+  filter(!(SocCode %in% join_1$SocCode),
+         !(SocCode %in% join_2$SocCode)) %>%
+  left_join(occupation_totals, by = c("Soc_last2" = "SOC3")) %>%
+  filter(!is.na(OCCSOC))
+join_4 <- oflc_alt %>%
+  filter(!(SocCode %in% join_1$SocCode),
+         !(SocCode %in% join_2$SocCode),
+         !(SocCode %in% join_3$SocCode)) %>%
+  left_join(occupation_totals, by = c("Soc_last2" = "SOC4")) %>%
+  filter(!is.na(OCCSOC))
+
+join_5 <- oflc_alt %>%
+  filter(!(SocCode %in% join_1$SocCode),
+         !(SocCode %in% join_2$SocCode),
+         !(SocCode %in% join_3$SocCode),
+         !(SocCode %in% join_4$SocCode)) %>%
+  left_join(occupation_totals, by = c("Soc_last1" = "Full_SOC")) %>%
+  filter(!is.na(OCCSOC))
+
+join_6 <- oflc_alt %>%
+  filter(!(SocCode %in% join_1$SocCode),
+         !(SocCode %in% join_2$SocCode),
+         !(SocCode %in% join_3$SocCode),
+         !(SocCode %in% join_4$SocCode),
+         !(SocCode %in% join_5$SocCode)) %>%
+  left_join(occupation_totals, by = c("Soc_last2" = "Full_SOC")) %>%
+  filter(!is.na(OCCSOC))
+
+join_7 <- oflc_alt %>%
+  filter(!(SocCode %in% join_1$SocCode),
+         !(SocCode %in% join_2$SocCode),
+         !(SocCode %in% join_3$SocCode),
+         !(SocCode %in% join_4$SocCode),
+         !(SocCode %in% join_5$SocCode),
+         !(SocCode %in% join_6$SocCode)) %>%
+  left_join(occupation_totals, by = c("Soc_last3" = "Full_SOC")) %>%
+  filter(!is.na(OCCSOC))
+
+
+
+no_join <- oflc_alt %>%
+  filter(!(SocCode %in% join_1$SocCode),
+         !(SocCode %in% join_2$SocCode),
+         !(SocCode %in% join_3$SocCode),
+         !(SocCode %in% join_4$SocCode),
+         !(SocCode %in% join_5$SocCode),
+         !(SocCode %in% join_6$SocCode),
+         !(SocCode %in% join_7$SocCode)) %>%
+  mutate(manual_soc = case_when(SocCode == "29-1221" ~ "29-1210",
+                                SocCode == "29-1222" ~ "29-1210",
+                                SocCode == "29-1223" ~ "29-1210",
+                                SocCode == "29-1224" ~ "29-1210",
+                                SocCode == "29-1229" ~ "29-1210",
+                                SocCode == "43-4151" ~ "43-4000",
+                                SocCode == "43-9199" ~ "43-9000",
+                                SocCode == "43-4199" ~ "43-4YYY",
+                                SocCode == "47-2171" ~ "47-2000", 
+                                SocCode == "51-4191" ~ "51-4000",
+                                SocCode == "51-4192" ~ "51-4000",
+                                SocCode == "51-4193" ~ "51-4000",
+                                SocCode == "51-4194" ~ "51-4000",
+                                SocCode == "51-4199" ~ "51-4000",
+                                TRUE ~ NA))
 
